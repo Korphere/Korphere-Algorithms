c878e562e9ef9bedf1eb8fbec5b3a2c2c1d8dc0d
KHRxnP <kozuka.mayumi@gmail.com>
2026-03-08 19:56:52 +0900

    Started Elixir, LLVM IR support

--- rust/sort/bubble_sort.rs
+++ rust/sort/bubble_sort.rs
@@ -1,27 +1,29 @@
-fn bubble_sort(target: Vec<i32>) -> Vec<i32> {
-    let targetc = target.clone();
-    for(let i in 0..targetc.len()){
-        for(let j in (i+1)..targetc.len()){
-            if(targetc[j] < targetc[j-1]){
+#[no_mangle]
+pub fn bubble_sort(target: Vec<i32>) -> Vec<i32> {
+    let mut targetc = target.clone();
+    for i in 0..targetc.len() {
+        for j in (i+1)..targetc.len() {
+            if targetc[j] < targetc[j-1] {
                 let tmp = targetc[j];
                 targetc[j] = targetc[j-1];
                 targetc[j - 1] =tmp;
             }
         }
     }
-    targetc;
+    targetc
 }
 
-fn bubble_sort_mut(target: Vec<i32>) -> Vec<i32> {
-    for(let i in 0..target.len()){
-        for(let j in (i+1)..target.len()){
-            if(target[j] < target[j-1]){
+#[no_mangle]
+pub fn bubble_sort_mut(mut target: Vec<i32>) -> Vec<i32> {
+    for i in 0..target.len() {
+        for j in (i+1)..target.len() {
+            if target[j] < target[j-1] {
                 let tmp = target[j];
                 target[j] = target[j-1];
                 target[j - 1] =tmp;
             }
         }
     }
-    target;
+    target
 }
 
